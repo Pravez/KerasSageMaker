8dfdf2d97b2f8b582f66b44bcc01efea11c8b464
Paul Breton <paul.ravezb@gmail.com>
2019-01-08 17:04:12 +0100

    Create dataset_to_s3.py

---
+++ dataset_to_s3.py
@@ -0,0 +1,56 @@
+import boto3
+import os
+import zipfile
+from PIL import Image
+
+s3 = boto3.client('s3')
+s3.create_bucket(Bucket='cats_n_dogs')
+
+zip_dataset = "kagglecatsanddogs_3367a.zip"
+tmp_dest = os.path.join(os.path.dirname(__file__),"tmp")
+data_dir = "~/cats_n_dogs_dataset/"
+
+if not os.path.exists(tmp_dest):
+    os.mkdir(tmp_dest)
+
+with zipfile.ZipFile(zip_dataset, 'r') as zip_ref:
+    zip_ref.extractall(tmp_dest)
+
+# The archive contains a main folder named "PetImages" followed by two folders "Cat" and "Dog"
+# We will take only ~ 1000 images for each class to stay light.
+
+dog_dir = tmp_dest + "PetImages/Dog"
+cat_dir = tmp_dest + "PetImages/Cat"
+
+train_dir = data_dir + "train/"
+validation_dir = data_dir + "validation/"
+
+os.mkdir(train_dir)
+os.mkdir(validation_dir)
+
+
+def move_and_check_files(origin, dest_train, dest_valid, quantity_train=1000, quantity_valid=200):
+    moved = 0
+    for file in os.listdir(origin):
+        try:
+            # Before moving the file we check if the image is readable
+            im = Image.open(os.path.join(origin, file))
+            im.verify()
+            os.rename(
+                os.path.join(origin, file),
+                os.path.join(dest_train if moved < quantity_train else dest_valid, file)
+            )
+            moved += 1
+        except Exception as e:
+            print("Bad image : %s" % file)
+
+        if moved >= quantity_train + quantity_valid:
+            break
+
+
+for category in [(cat_dir, 'cat'), (dog_dir, 'dog')]:
+    train = train_dir + category[1]
+    valid = validation_dir + category[1]
+    os.mkdir(train)
+    os.mkdir(valid)
+    move_and_check_files(category[0], train, valid)
