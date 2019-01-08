8bc28e3ee2feaea6d6bc589d9ec95bb1d0f6b2b9
Paul Breton <paul.ravezb@gmail.com>
2019-01-08 16:37:25 +0100

    Create aws_job.py

---
+++ aws_job.py
@@ -0,0 +1,50 @@
+import os
+import sagemaker
+import numpy as np
+from sagemaker.tensorflow import TensorFlow
+from tensorflow.python.keras.preprocessing.image import load_img
+
+ON_SAGEMAKER_NOTEBOOK = False
+
+sagemaker_session = sagemaker.Session()
+if ON_SAGEMAKER_NOTEBOOK:
+    role = sagemaker.get_execution_role()
+else:
+    role = "[The ARN of your role]"
+
+
+bucket = "sagemaker-tests"
+key = "data"
+key_output = "output"                   # Path from the bucket's root to the dataset
+train_instance_type='ml.p2.xlarge'      # The type of EC2 instance which will be used for training
+deploy_instance_type='ml.p2.xlarge'     # The type of EC2 instance which will be used for deployment
+hyperparameters={
+    "learning_rate": 1e-4,
+    "decay": 1e-6
+}
+
+train_input_path = "s3://{}/{}/train/".format(bucket, key)
+validation_input_path = "s3://{}/{}/validation/".format(bucket, key)
+
+estimator = TensorFlow(
+  entry_point=os.path.join(os.path.dirname(__file__), "cats_n_dogs.py"),             # Your entry script
+  role=role,
+  framework_version="1.12.0",               # TensorFlow's version
+  hyperparameters=hyperparameters,
+  training_steps=1000,
+  evaluation_steps=100,
+  train_instance_count=1,                   # "The number of GPUs instances to use"
+  train_instance_type=train_instance_type,
+)
+
+estimator.fit({'training': train_input_path, 'eval': validation_input_path})
+
+predictor = estimator.deploy(initial_instance_count=1, instance_type=deploy_instance_type)
+
+
+cat_image = load_img("[YOUR_FOLDER]/cat/2282.jpg", target_size=(128, 128))
+cat_image_array = np.array(cat_image).reshape((1, 128, 128))
+
+print(predictor.predict({'inputs_input': cat_image_array}))
+
+sagemaker.Session().delete_endpoint(predictor.endpoint)
