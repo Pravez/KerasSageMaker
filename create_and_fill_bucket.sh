38680792c8ace867a1c7d161e485e17e5d9f1d52
Paul Breton <paul.ravezb@gmail.com>
2019-01-09 11:08:58 +0100

    Update create_and_fill_bucket.sh

--- create_and_fill_bucket.sh
+++ create_and_fill_bucket.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-aws s3api create-bucket --bucket cats_n_dogs --acl public read --region eu-west-1
-aws s3 cp ~/cats_n_dogs s3://cats_n_dogs/data --recursive
+# You could also create the bucket from the CLI with this command
+# aws s3api create-bucket --bucket cats-n-dogs --acl public read --region eu-west-1
+# Then send to S3
+aws s3 cp ./cats_n_dogs s3://cats-n-dogs/data --recursive
